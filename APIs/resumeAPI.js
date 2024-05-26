7ae7d4a1a8d995fff10d6262ec63ea23c0bca884
shashidhar7682 <shashidhar7682@gmail.com>
2024-05-26 21:20:20 +0530

    commit_26/05/24_21:20

--- APIs/resumeAPI.js
+++ APIs/resumeAPI.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const PDFParser = require('pdf-parse');
 const multer = require('multer');
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const axios = require('axios');
+//// const { GoogleGenerativeAI } = require("@google/generative-ai");//for google gen ai model
 
 const resumeApp = express.Router();
 const upload = multer();
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+//// const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const COHERE_API_KEY = process.env.COHERE_API_KEY;
 
 // API endpoint to parse resume file
 resumeApp.post('/parse-resume', upload.single('resume'), async (req, res) => {
@@ -20,17 +22,40 @@ resumeApp.post('/parse-resume', upload.single('resume'), async (req, res) => {
         // Parse the uploaded resume file
         const resumeText = await parseResume(pdfBuffer);
         console.log(resumeText)
-        // For text-only input, use the gemini-pro model
-        const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-        const prompt = `This is a resume categorize this in to sections in json format without any acknowledgements, only return the json data:
+        const prompt = `Following will be a resume text categorize this in to sections in json format without any acknowledgements, only return the json data for the following text:
         ${resumeText}`
 
-        const result = await model.generateContent(prompt);
-        const response = await result.response;
-        const text = response.text();
-        console.log(text);
-        res.send(text);
+        ////USING Google Gen AI
+
+        
+        // For text-only input, use the gemini-pro model
+        //const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+        // const result = await model.generateContent(prompt);
+        // const response = await result.response;
+        // const text = response.text();
+        // console.log(text);
+        // res.send(text);
+        
+
+        // Using Cohere API
+        const response = await axios.post('https://api.cohere.ai/generate', {
+            prompt: prompt,
+            max_tokens: 5000,
+        }, {
+            headers: {
+                'Authorization': `Bearer ${COHERE_API_KEY}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        const genText=response.data.text.replace(/```/g, '').trim()
+
+        const jsonStartIndex = genText.indexOf('{');
+        const jsonEndIndex = genText.lastIndexOf('}') + 1;
+        const jsonString = genText.substring(jsonStartIndex, jsonEndIndex);
+        
+        const jsonObject = JSON.parse(jsonString);
+        res.json(jsonObject);
     } catch (error) {
         console.error('Error processing resume:', error);
         res.status(500).send('Error processing resume');
